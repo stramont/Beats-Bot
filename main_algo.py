69a39a049fae319b8f59729ef8ea5af9974acf0e
Sebastian Tramontana <stramont@umd.edu>
2021-11-06 14:33:03 -0400

    main-algo

---
+++ main_algo.py
@@ -0,0 +1,31 @@
+#Genetic algorithm
+
+import numpy as np
+
+class main_algo:
+    
+    def __init__(self, stringLength, popSize, nGens, pm, pc):
+        #Initialize variables
+        
+    def fitFcn(self, pop):
+        #compute fitness of population    
+        
+    def fitness_of_chrome(self, chrome):
+        #Compute the fitness of a single chomosome
+        return 1
+
+    
+    def crossover(self, child1, child2):
+        #Compute the crossover between two children/chromosomes
+
+    def mutate(self, chrome):
+        #Mutate a chromosome
+
+    def tournament(self, pop, fitness, popsize):
+        #Conduct a tournament to select offspring of population
+
+    def elitism(self):
+        #Find the best of the best of the population
+
+    def run_algo(self):
+        #run the algorithm
