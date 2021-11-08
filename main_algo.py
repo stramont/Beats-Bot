6b9f82e23c7f8633828960823a9a43ba6a3e3868
r-gray123 <54861302+r-gray123@users.noreply.github.com>
2021-11-08 18:00:05 -0500

    Implemented elitism, tournament, and fitFcn

--- main_algo.py
+++ main_algo.py
@@ -14,8 +14,14 @@ class algo:
         self.pm = pm
         
     def fitFcn(self, pop):
-        #compute fitness of population    
-        return 1
+        #compute fitness of population
+        #as a numpy array of the fitness of each chromosome
+        pop_fitness = []
+
+        for chrome in pop:
+            pop_fitness.append(fitness_of_chrome(chrome))
+
+        return np.array(pop_fitness)
         
     def fitness_of_chrome(self, chrome):
         #Compute the fitness of a single chromosome
@@ -42,13 +48,81 @@ class algo:
         pop[whereMutate] = 1 - pop[whereMutate]
         return pop #returns population with mutations
 
-    def tournament(self, pop, fitness, popsize):
+    def tournament(self, pop, popsize, tourn_size=2):
         #Conduct a tournament to select offspring of population
-        return 1
 
-    def elitism(self):
+        cand_list1 = []
+        cand_list2 = []
+        cand_fit1 = []
+        cand_fit2 = []
+
+        #Choose the index of the tournament candidates
+        #as many as were specified
+        curr_cands = 0
+        while curr_cands < tourn_size:
+            cand_index = np.random.randint(popsize)
+            
+            if cand_index in cand_list1:
+                continue
+            else:
+                cand_list1.append(cand_index)
+                curr_cands += 1
+        
+        #Compare fitnesses of all the candidates and pick the most fit
+        for ci in cand_list1:
+            cand_fit1.append(fitness_of_chrome(pop[ci]))
+
+        cl_array = np.array(cand_list1)
+        ind = cl_array.argmax() #Get the indices of the maximum fitness
+        par1_index = ind[0]   #Since argmax() returns an array, get the first value
+
+        #After choosing the first parent, choose the second
+        curr_cands = 0
+        while curr_cands < tourn_size:
+            cand_index = np.random.randint(popsize)
+
+            if cand_index in cand_list2:
+                continue
+            else:
+                cand_list2.append(cand_index)
+                curr_cands += 1
+
+        #Compare the fitness of all the candidates and pick the most fit
+        for ci in cand_list2:
+            cand_fit2.append(fitness_of_chrome(pop[ci]))
+
+        cl_array = np.array(cand_list2)
+        ind = cl_array.argmax()
+        par2_index = ind[0]
+        
+
+        #Return the parent indices or the parent chromosomes themselves?
+        return par1_index, par2_index
+
+    def elitism(self, pop):
         #Find the best of the best of the population
-        return 1
+        pop_prob = []
+        pop_fit = []
+
+        #Get the fitness for every chromosome
+        #NOTE: for this and the probability list pop_prob, the index
+        #of the chromosome in pop corresponds to the index in these
+        #two lists.
+        for chrome in pop:
+            pop_fit.append(fitness_of_chrome(chrome))
+
+        fit_array = np.array(pop_fit) #Make sure it's a numpy array
+        fit_sum = fit_array.sum() #Get the sum of the fitness values
+
+        #Calculate the probability to  be chosen for each chromosome
+        for fitness in fit_array:
+            pop_prob.append(fitness / fit_sum)
+
+        #Randomly choose a chromosome based on their given probabilities
+        best_index = np.random.choice(pop, 1, pop_prob)
+        
+        #Return the randomly chosen best fit chromosome
+        return pop[best_index]
 
     def run_algo(self): #run the algorithm
         fid = self.fid #output file
