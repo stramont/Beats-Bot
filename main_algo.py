425c241e5e7eb83543b7e45436f61958e1076639
Sebastian Tramontana <stramont@umd.edu>
2021-11-08 18:49:35 -0500

    .

--- main_algo.py
+++ main_algo.py
@@ -12,6 +12,26 @@ class algo:
         self.stringLength = stringLength
         self.nGens = nGens
         self.pm = pm
+        self.pc = pc
+        if np.mod(popSize,2)==0:           # popSize must be even
+                self.popSize = popSize
+        else:
+                self.popSize = popSize+1
+        self.pop = np.random.rand(self.popSize,self.stringLength)
+        self.pop = np.where(self.pop<0.5,1,0)  # create initial pop
+        fitness = self.fitFcn(self.pop)    # fitness values for initial population
+        self.bestfit = fitness.max()       # fitness of (first) most fit chromosome
+        self.bestloc = np.where(fitness == self.bestfit)[0][0]  # most fit chromosome locn
+        self.bestchrome = self.pop[self.bestloc,:]              # most fit chromosome
+        self.bestfitarray = np.zeros(self.nGens + 1)  # array of max fitness vals each generation
+        self.bestfitarray[0] = self.bestfit           #  (+ 1 for init pop plus nGens)
+        self.meanfitarray = np.zeros(self.nGens + 1)  # array of mean fitness vals each generation
+        self.meanfitarray[0] = fitness.mean()
+        fid.write("popSize: {}  nGens: {}  pm: {}  pc: {}\n".format(popSize,nGens,pm,pc))
+        fid.write("initial population, fitnesses: (up to 1st 100 chromosomes)\n")
+        for c in range(min(100,popSize)):   # for each of first 100 chromosomes 
+           fid.write("  {}  {}\n".format(self.pop[c,:],fitness[c]))
+        fid.write("Best initially:\n  {} at locn {}, fitness = {}\n".format(self.bestchrome,self.bestloc,self.bestfit))
         
     def fitFcn(self, pop):
         #compute fitness of population
@@ -19,7 +39,7 @@ class algo:
         pop_fitness = []
 
         for chrome in pop:
-            pop_fitness.append(fitness_of_chrome(chrome))
+            pop_fitness.append(self.fitness_of_chrome(chrome))
 
         return np.array(pop_fitness)
         
@@ -48,18 +68,19 @@ class algo:
         pop[whereMutate] = 1 - pop[whereMutate]
         return pop #returns population with mutations
 
-    def tournament(self, pop, popsize, tourn_size=2):
+    def tournament(self, pop, tourney_size=2):
         #Conduct a tournament to select offspring of population
 
         cand_list1 = []
         cand_list2 = []
         cand_fit1 = []
         cand_fit2 = []
+        popsize = len(pop)
 
         #Choose the index of the tournament candidates
         #as many as were specified
         curr_cands = 0
-        while curr_cands < tourn_size:
+        while curr_cands < tourney_size:
             cand_index = np.random.randint(popsize)
             
             if cand_index in cand_list1:
@@ -70,15 +91,14 @@ class algo:
         
         #Compare fitnesses of all the candidates and pick the most fit
         for ci in cand_list1:
-            cand_fit1.append(fitness_of_chrome(pop[ci]))
+            cand_fit1.append(self.fitness_of_chrome(pop[ci]))
 
         cl_array = np.array(cand_list1)
-        ind = cl_array.argmax() #Get the indices of the maximum fitness
-        par1_index = ind[0]   #Since argmax() returns an array, get the first value
-
+        par1_index = cl_array.argmax() #Get the indices of the maximum fitness
+        
         #After choosing the first parent, choose the second
         curr_cands = 0
-        while curr_cands < tourn_size:
+        while curr_cands < tourney_size:
             cand_index = np.random.randint(popsize)
 
             if cand_index in cand_list2:
@@ -89,17 +109,16 @@ class algo:
 
         #Compare the fitness of all the candidates and pick the most fit
         for ci in cand_list2:
-            cand_fit2.append(fitness_of_chrome(pop[ci]))
+            cand_fit2.append(self.fitness_of_chrome(pop[ci]))
 
         cl_array = np.array(cand_list2)
-        ind = cl_array.argmax()
-        par2_index = ind[0]
+        par2_index = cl_array.argmax()
         
 
         #Return the parent indices or the parent chromosomes themselves?
         return par1_index, par2_index
 
-    def elitism(self, pop):
+    def fitpselect(self, pop):
         #Find the best of the best of the population
         pop_prob = []
         pop_fit = []
@@ -109,7 +128,7 @@ class algo:
         #of the chromosome in pop corresponds to the index in these
         #two lists.
         for chrome in pop:
-            pop_fit.append(fitness_of_chrome(chrome))
+            pop_fit.append(self.fitness_of_chrome(chrome))
 
         fit_array = np.array(pop_fit) #Make sure it's a numpy array
         fit_sum = fit_array.sum() #Get the sum of the fitness values
@@ -122,12 +141,63 @@ class algo:
         best_index = np.random.choice(pop, 1, pop_prob)
         
         #Return the randomly chosen best fit chromosome
-        return pop[best_index]
+        return best_index
 
-    def run_algo(self): #run the algorithm
+    def elitism(self, pop, num_of_top):
+        #Get the top chromosome (indexes) equal to 'num_of_top'
+        fit_list = []
+        index_list = []
+        for chrome in pop:
+            fit_list.append(self.fitness_of_chrome(chrome))
+
+        fit_list = np.array(fit_list)
+
+        for _ in range(num_of_top):
+            bf_index = fit_list.argmax()
+            index_list.append(bf_index)
+
+
+        return index_list
+
+    def run_algo(self,elite_size=0,tournament=True,tourney_size=2): #run the algorithm
         fid = self.fid #output file
+        
         for gen in range(self.nGens):
-            print("loop")
+            # initialize new population
+            newPop = np.zeros((self.popSize,self.stringLength),dtype = 'int64')
+            # create new population newPop via selection and crossovers with prob pc
+            for pair in range (0,self.popSize-elite_size,2):
+                #tournament or fitpselect
+                if tournament:
+                    # p1, p2 integers indexes for the chromosomes in self.pop
+                    p1, p2 = self.tournament(self.pop,tourney_size) 
+                else:
+                    p1 = self.fitpselect(self.pop) 
+                    p2 = self.fitpselect(self.pop)
+                child1 = np.copy(self.pop[p1,:])       # child1 for newPop
+                child2 = np.copy(self.pop[p2,:])       # child2 for newPop
+                if np.random.rand() < self.pc:                 # with prob self.pc 
+                    child1, child2 = self.crossover(child1,child2)  #   do crossover
+                newPop[pair,:] = child1                # add offspring to newPop
+                newPop[pair + 1,:] = child2
+
+            # mutations to population with probability pm
+            newPop = self.mutate(newPop)
+
+            #Elitism: Select the top fitness chromosomes for the next gen
+            top_chrome = self.elitism(self.pop, elite_size)
+            pop_index=self.popSize-elite_size
+            for index in top_chrome:
+                newPop[pop_index,:] = self.pop[index]
+                pop_index+=1
+
+            self.pop = newPop
+            fitness = self.fitFcn(self.pop)    # fitness values for population
+            self.bestfit = fitness.max()       # fitness of (first) most fit chromosome
+            self.bestloc = np.where(fitness == self.bestfit)[0][0]  # most fit chromosome locn
+            self.bestchrome = self.pop[self.bestloc,:]              # most fit chromosome
+            if (np.mod(gen,10)==0):            # print epoch, max fitness
+                 print("generation: ",gen+1,"max fitness: ",self.bestfit) 
         fid.write("\nfinal population, fitnesses: (up to 1st 100 chromosomes)\n")
         fitness = self.fitFcn(self.pop) #compute final pop fitness
         self.bestfit = fitness.max() #fitness of first most fit chromosome
--- main_driver.py
+++ main_driver.py
@@ -2,4 +2,4 @@ import main_algo
 
 gen_algo = main_algo.algo(320, 50, 50, .01, .05)
 
-gen_algo.run_algo()
+gen_algo.run_algo(5)
