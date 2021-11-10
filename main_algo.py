4f0eb883d57fa1e343757525a7cf4b20395a3721
r-gray123 <54861302+r-gray123@users.noreply.github.com>
2021-11-10 18:35:50 -0500

    Fixed error in fitpselect

--- main_algo.py
+++ main_algo.py
@@ -127,13 +127,15 @@ class algo:
         #Find the best of the best of the population
         pop_prob = []
         pop_fit = []
+        pop_indices = []
 
         #Get the fitness for every chromosome
         #NOTE: for this and the probability list pop_prob, the index
         #of the chromosome in pop corresponds to the index in these
         #two lists.
-        for chrome in pop:
+        for i, chrome in enumerate(pop):
             pop_fit.append(self.fitness_of_chrome(chrome))
+            pop_indices.append(i)
 
         fit_array = np.array(pop_fit) #Make sure it's a numpy array
         fit_sum = fit_array.sum() #Get the sum of the fitness values
@@ -143,7 +145,7 @@ class algo:
             pop_prob.append(fitness / fit_sum)
 
         #Randomly choose a chromosome based on their given probabilities
-        best_index = np.random.choice(pop, 1, pop_prob)
+        best_index = np.random.choice(pop_indices, 1, pop_prob)
         
         #Return the randomly chosen best fit chromosome
         return best_index
