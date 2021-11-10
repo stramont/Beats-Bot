35ad579ac6992321b50630fa72344655bcfbb04b
Sebastian Tramontana <stramont@umd.edu>
2021-11-10 17:53:01 -0500

    comments on main driver

--- main_driver.py
+++ main_driver.py
@@ -1,5 +1,12 @@
 import main_algo
 
-gen_algo = main_algo.algo(320, 50, 50, .01, .05)
+#Main driver for the algorithm
 
+#initialize algo object:
+gen_algo = main_algo.algo(320, 50, 50, .01, .05)
+# stringLength, popSize, nGens, pm, pc
+
+#Run algorithm
 gen_algo.run_algo(5)
+# Arguments optional: elite_size, tournament, tourney_size
+# default arguments: 2, True, 2
